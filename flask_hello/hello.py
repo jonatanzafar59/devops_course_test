742d371c0df85996d335fd2427184f184d480803
jonathan <jonatanzafar59@gmail.com>
2017-07-17 17:55:18 +0300

    hello

--- flask_hello/hello.py
+++ flask_hello/hello.py
@@ -3,10 +3,9 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/hello', methods=['POST'])
+@app.route('/hello', methods=['GET'])
 def print_hello():
-    my_param = request.json
-    return json.dumps(my_param)
+    return 'Hello'
 
 app.run(host='0.0.0.0', port=5555)
 
